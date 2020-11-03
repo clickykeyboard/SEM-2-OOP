62e2896cfac58065cbad350ccb2ffce8b5fb1fa8
cloudswritecode <badrkmc@hotmail.com>
2020-11-03 18:44:38 +0500

    added practice file

---
+++ OOP/practice.py
@@ -0,0 +1,15 @@
+class Human:
+
+    def __init__(self, name, eyes, hands):
+        self.name = name
+        self.eyes = eyes
+        self.hands = hands
+
+person = Human("Khadija", 2, 2)
+person1 = Human("Zeeshan", 3, 2)
+
+print(person.name)
+print(person.eyes)
+
+print(person1.eyes)
+print(person1.hands)
