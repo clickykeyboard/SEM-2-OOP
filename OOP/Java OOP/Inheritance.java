5e45611f50925eed73b823ccc037e91212bd7a8e
cloudswritecode <badrkmc@hotmail.com>
2020-11-18 11:04:41 +0500

    inheritance week 07

---
+++ OOP/Java OOP/Inheritance.java
@@ -0,0 +1,15 @@
+class SuperClass {
+
+    public SuperClass() {
+        System.out.println("Creating super class!");
+    }
+
+    public void superClassMethod() {
+        System.out.println("Printing superClassMethod!");
+    }
+
+}
+
+class BaseClass extends SuperClass {
+
+}
--- OOP/Java OOP/Main.java
+++ OOP/Java OOP/Main.java
@@ -2,12 +2,14 @@ class Main {
 
     public static void main(String[] args) {
 
-        Clock clock = new Clock();
+        // Clock clock = new Clock();
 
-        clock.hours.setValue(0);
-        clock.minutes.setValue(0);
+        // clock.hours.setValue(0);
+        // clock.minutes.setValue(0);
 
-         
+        BaseClass baseClass = new BaseClass();
+        baseClass.superClassMethod();
+        
     }
 
 
