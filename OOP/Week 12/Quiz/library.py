6c440704ca40cbd6d23bcb83509cf83592355251
cloudswritecode <badrkmc@hotmail.com>
2020-12-23 12:35:17 +0500

    na hi pata hai na hi koi puche

---
+++ OOP/Week 12/Quiz/library.py
@@ -0,0 +1,25 @@
+class Library:
+
+    def __init__(self):
+
+        self.books = []
+        self.patrons = []
+
+    def get_books(self):
+        return self.books
+
+    def get_patrons(self):
+        return self.patrons
+
+    def add_book(self, book):
+        self.books.append(book)
+
+    def remove_book(self, book):
+        try:
+            self.books.remove(book)
+            book.checked_out = False
+        except:
+            pass
+
+    def add_patron(self, patron):
+        self.patrons.append(patron)
---
+++ OOP/Week 12/Quiz/manager.py
@@ -0,0 +1,76 @@
+from library import Library
+from patron import Patron
+from book import Book
+
+
+class Manager(Library):
+
+    def __init__(self):
+
+        self.choices = [1, 2]
+        super().__init__()
+
+    def run_menu(self):
+        while True:
+
+            print("""
+---------------------
+Welcome to the Library
+1 - Borrow a book
+2 - Return a book
+---------------------     
+                """)
+
+            choice = int(input("Enter your choice: "))
+
+            if choice not in self.choices:
+                print("Please enter a valid choice")
+
+            if choice == 1:
+
+                print("\nBorrowing a book...")
+                patron_name = input("Enter your name: ")
+                patron = Patron(patron_name)
+                super().add_patron(patron)
+
+                book_name = input("Enter book name: ")
+                book_author = input("Enter book author: ")
+
+                for book in super().get_books():
+                    if book.is_checked_out():
+                        print("Book already checked out")
+                        print("Adding to waitlist...")
+                        book.add_to_waitlist(patron)
+                        break
+
+                book = Book(book_name, book_author)
+                super().add_book(book)
+
+                print(
+                    f"-----\n"
+                    f"Book borrowed: \nName: {book_name}\nBook author: {book_author}"
+                )
+
+            elif choice == 2:
+
+                print("\nReturning a book...")
+                patron_name = input("Enter your name: ")
+                book_name = input("Enter book name: ")
+
+                for patron in super().get_patrons():
+                    if patron.get_name() == patron_name:
+
+                        for book in super().get_books():
+                            if book.get_book_name() == book_name:
+                                patron.return_book(book)
+                                print("Book returned successfully")
+                                break
+                            else:
+                                print("Could not find your book")
+                    else:
+                        print(
+                            "You have not registered as a patron yet\nPlease try again")
+
+
+manager = Manager()
+manager.run_menu()
