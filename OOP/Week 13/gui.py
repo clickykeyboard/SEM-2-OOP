6684d88d0669f8544c3cc72dcd3021e8660ad169
cloudswritecode <badrkmc@hotmail.com>
2021-01-21 16:11:24 +0500

    week 13 gui py

--- OOP/Week 13/gui.py
+++ OOP/Week 13/gui.py
@@ -1,14 +1,17 @@
 import tkinter
 
+
 class GUI:
 
     def __init__(self):
         self.count = 0
+        self.texts = []
         self.main_window = tkinter.Tk()
         self.input_string = tkinter.StringVar()
         self.main_window.title("My window")
 
-        self.name = tkinter.Label(text="Badr ", foreground="red", background="yellow")
+        self.name = tkinter.Label(
+            text="Badr ", foreground="red", background="yellow")
         self.name.pack()
 
         self.age = tkinter.Label(text="20")
@@ -16,19 +19,19 @@ class GUI:
 
         self.button = tkinter.Button(
             text="Click me",
-            background="yellow", 
+            background="yellow",
             foreground="black",
             command=self.on_button_click
-            )
-        
+        )
+
         self.button.pack()
-        
+
         self.entry = tkinter.Entry(textvariable=self.input_string)
         self.entry.pack()
 
         self.quit = tkinter.Button(
             text="Quit",
-            command=self.quit_window
+            command=self.main_window.destroy
         )
         self.quit.pack()
 
@@ -36,9 +39,9 @@ class GUI:
 
     def on_button_click(self):
         print("Button clicked")
-        self.age.config(text=self.input_string.get())
+        self.texts.append(self.input_string.get())
+        self.entry.delete(0, "end")
+        self.age.config(text="\n".join(self.texts))
 
-    def quit_window(self):
-        self.main_window.destroy()
 
 gui = GUI()
